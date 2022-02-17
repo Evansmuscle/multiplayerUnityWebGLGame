01917700314bff8d860c5297802cdbe14f8278a7
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-02-17 14:09:51 +0300

    Commit the initial prototype of the game

---
+++ README.md
@@ -0,0 +1,1 @@
+To play the game you must first install all the npm requirements, then in the console type "npm start", and open "http://localhost:5500" in your browser to start playing the game.
---
+++ index.js
@@ -0,0 +1,19 @@
+import compression from "compression";
+import express, { json, urlencoded } from "express";
+
+const app = express();
+
+const appConfig = {
+  port: 5500,
+  hostname: "localhost",
+};
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(compression());
+
+app.use(express.static("client"));
+
+app.listen(appConfig.port, appConfig.hostname, () => {
+  console.log("Server started");
+});
